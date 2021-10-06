5d286be4a4c47e5e6d00ac3ae6c6adcb62e84b99
Nikhil Gupta <nikhil@piktorlabs.com>
2021-10-06 13:55:41 +0530

    navbar styling

--- src/parts/navbar/navBar.jsx
+++ src/parts/navbar/navBar.jsx
@@ -1,40 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.svg";
 import "./navbar.css";
 
 const NavBar = () => {
+  const [collapse, setCollapse] = useState(true);
+  const navItems = [
+    { label: "Safes", link: "/safes" },
+    { label: "Vault AppRoles", link: "/vault" },
+    { label: "Service Accounts", link: "/" },
+    { label: "IAM Service Accounts", link: "/" },
+    { label: "Azure Active Directory", link: "/" },
+  ];
+
+  const handleCollapse = () => {
+    setCollapse((c) => !c);
+  };
+
   return (
     <header className="nav">
       <Link className="nav__logo" to="/">
         <img src={logo} alt="t-vault" />
       </Link>
-      <ul className="list list-nav">
-        <li className="list__item-nav">
-          <Link className="nav__item" to="/safes">
-            Safes
-          </Link>
-        </li>
-        <li className="list__item-nav">
-          <Link className="nav__item" to="/vault">
-            Vault AppRoles
-          </Link>
-        </li>
-        <li className="list__item-nav">
-          <Link className="nav__item" to="/">
-            Service Accounts
-          </Link>
-        </li>
-        <li className="list__item-nav">
-          <Link className="nav__item" to="/">
-            IAM Service Accounts
-          </Link>
-        </li>
-        <li className="list__item-nav">
-          <Link className="nav__item" to="/">
-            Azure Active Directory
-          </Link>
-        </li>
+      <i
+        className="fa fa-bars fa-2x nav__toggle"
+        aria-hidden="true"
+        onClick={() => handleCollapse()}
+      ></i>
+      <ul
+        className={`list nav__list${collapse ? " nav__list--collapsed" : ""}`}
+      >
+        {navItems.map((navItem, index) => (
+          <li className="nav__item" key={index}>
+            <Link to={navItem.link}>{navItem.label}</Link>
+          </li>
+        ))}
       </ul>
     </header>
   );
